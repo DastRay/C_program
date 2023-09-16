4b2cf6dc1c13b04adadfbeaafdf5e243135de18f
DastRay <104367556+DastRay@users.noreply.github.com>
2023-09-16 12:56:14 +0300

    Add files via upload

---
+++ Euler.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/*
+* @breaf Найдите сумму чисел меньше value_, кратных multipies_
+* @param value_: число, меньше котрого ищем сумму
+* @param multipies_: массив кратных чисел
+* @return сумма чисел
+*/
+long long Problem_1( const int value_, const int* multiples_);
---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"function.h"
+#include "Euler.h"
+
+#define SIZE 2
+
+// каждый раз когда в коде встречается N, оно будет менятся на заданое значение
+
+int main()
+{
+	int value = 0;
+	int array[SIZE];
+
+	printf("value = ");
+	scanf_s("%d", &value);
+
+	printf("array = ");
+	for (unsigned int i = 0; i < SIZE; ++i)
+		scanf_s("%d", &array[i]);
+
+	printf("answer = %llu\n", Problem_1(1000, array, SIZE));
+
+	return 0;
+}
